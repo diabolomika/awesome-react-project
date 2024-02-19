52da15c3f953f5b635988b2319f580461f53c498
Meike E <diabolomika@gmail.com>
2024-02-19 08:44:04 +0000

    edited api keys

--- react-weather-app-main/src/api.js
+++ react-weather-app-main/src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
+    "X-RapidAPI-Key": "45fb9f85bamsh97027dd98cbb840p116703jsn792136e95fcc",// enter your rapid api key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "29aabf643a8e0158db4edbdf363b6baf"; // enter your key from openweather API
